e0fa024164a6b712e63cf7e144f76b17419aa217
Denis <denis.tanjingyu@hotmail.sg>
2022-02-10 14:09:53 +0800

    Add py file

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,26 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Define a dictionary to store roman numerals and their respective values
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        # Define a variable to store the integer
+        integer = 0
+        
+        # Loop through s till second last character
+        # For each character, compare to next character (minus if less, add if more)
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                integer -= roman[s[i]]
+            else:
+                integer += roman[s[i]]
+        
+        # Last character is always added as it can never be less than non-existent "next"
+        return integer + roman[s[-1]]
