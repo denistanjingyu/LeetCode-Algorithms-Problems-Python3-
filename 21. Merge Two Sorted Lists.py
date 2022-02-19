7f72c8705712ffb8b350b9fa915cde42c9f0a713
Denis <denis.tanjingyu@hotmail.sg>
2022-02-19 22:11:40 +0800

    Add py file

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # Return the head of list1 or list2, if one is non-empty
+        if not list1 or not list2:
+            return list1 or list2
+        
+        # Recursively add sequential values to list1 if it contains smallest value
+        # Vice versa for list2
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
