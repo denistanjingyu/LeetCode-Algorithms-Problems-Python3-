d036f132492ba75225e47e3283be8032a15ea711
Denis <denis.tanjingyu@hotmail.sg>
2022-03-31 14:51:05 +0800

    Add py file

---
+++ 136. Single Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict_nums = {}
+        for i in nums:
+            if i not in dict_nums:
+                dict_nums[i] = 1
+            else:
+                dict_nums[i] += 1
+        
+        for num, count in dict_nums.items():
+            if count == 1:
+                return num
