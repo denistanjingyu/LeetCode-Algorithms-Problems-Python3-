46b73f6b8bf68933f60222050d25c9463c0dd3a7
Denis <denis.tanjingyu@hotmail.sg>
2022-04-11 14:48:17 +0800

    Add py file

---
+++ 202. Happy Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        memory_set = set()
+        while n not in memory_set:
+            memory_set.add(n)
+            n = sum([int(i)**2 for i in str(n)])
+        return n == 1
