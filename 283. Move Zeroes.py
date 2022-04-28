c0f31e535adca5a4f2f38fdcb54d0eca7ef6160c
Denis <denis.tanjingyu@hotmail.sg>
2022-04-28 17:07:18 +0800

    Add py file

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        anchor = 0
+        
+        for explorer in range(len(nums)):
+            if nums[explorer] != 0:
+                nums[anchor], nums[explorer] = nums[explorer], nums[anchor]
+                anchor += 1
