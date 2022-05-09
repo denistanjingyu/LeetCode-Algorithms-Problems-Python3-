a7290f9721546a09b2fc75f5bf0d218838a11c7e
Denis <denis.tanjingyu@hotmail.sg>
2022-05-09 23:42:52 +0800

    Add py file

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dict_store = {}
+        res = []
+        
+        for i in nums1:
+            if i not in dict_store:
+                dict_store[i] = 1
+            else:
+                dict_store[i] += 1
+                
+        for j in nums2:
+            if j in dict_store and dict_store[j] > 0:
+                res.append(j)
+                dict_store[j] -= 1
+        
+        return res
